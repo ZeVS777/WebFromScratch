94018d9c750bdb01b3542e3feecccc181405297e
ZeVS777 <zevs7772006@gmail.com>
2015-11-08 21:52:34 +0300

    Настройка движка представлений

---
+++ GlobalMvcHelpers/ViewEngines/CSharpRazorViewEngine.cs
@@ -0,0 +1,62 @@
+﻿using System.Web.Mvc;
+
+namespace GlobalMvcHelpers.ViewEngines
+{
+    /// <summary>
+    /// Движок представлений, который используется для формирования разметки Web-страницы на стороне сервера, используя синтаксис ASP.NET Razor
+    ///             и формат файлов C# .cshtml. <see cref="T:System.Web.Mvc.RazorViewEngine"/> поддерживает C# и VB и ищет файлы обоих фоматов .cshtml и .vbhtml.
+    ///             Данная версия ищет только .cshtml файлы, таким образом ускоряя процесс нахождения нужного файла.
+    /// 
+    /// </summary>
+    public class CSharpRazorViewEngine : RazorViewEngine
+    {
+        /// <summary>
+        /// Инициализация класса <see cref="T:GlobalMvcHelpers.ViewEngines.CSharpRazorViewEngine"/>.
+        /// 
+        /// </summary>
+        public CSharpRazorViewEngine()
+        {
+            // Пути поиска представлений Area
+            this.AreaViewLocationFormats = new string[2]
+            {
+                "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
+            };
+            // Пути поиска мастер представлений Area
+            this.AreaMasterLocationFormats = new string[2]
+            {
+                "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
+            };
+            // Пути поиска частичных представлений Area
+            this.AreaPartialViewLocationFormats = new string[2]
+            {
+                "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
+            };
+            // Пути поиска представлений
+            this.ViewLocationFormats = new string[2]
+            {
+                "~/Views/{1}/{0}.cshtml",
+                "~/Views/Shared/{0}.cshtml"
+            };
+            // // Пути поиска мастер представлений
+            this.MasterLocationFormats = new string[2]
+            {
+                "~/Views/{1}/{0}.cshtml",
+                "~/Views/Shared/{0}.cshtml"
+            };
+            // Пути поиска частичных представлений
+            this.PartialViewLocationFormats = new string[2]
+            {
+                "~/Views/{1}/{0}.cshtml",
+                "~/Views/Shared/{0}.cshtml"
+            };
+            // Расширение файлов представлений
+            this.FileExtensions = new string[1]
+            {
+                "cshtml"
+            };
+        }
+    }
+}
--- WebFromScratch/Global.asax.cs
+++ WebFromScratch/Global.asax.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Web.Mvc;
+using GlobalMvcHelpers.ViewEngines;
 
 namespace WebFromScratch
 {
     public class Global : System.Web.HttpApplication
     {
         #region Events
+        // Событие, срабатываемое перед запуском основного приложения
         protected void Application_Start(object sender, EventArgs e)
         {
-
+            ConfigureViewEngines();
         }
 
         protected void Session_Start(object sender, EventArgs e)
@@ -40,5 +43,17 @@ namespace WebFromScratch
 
         }
         #endregion
+
+        /// <summary>
+        /// Конфигурирование Движков представлений (View Engines).
+        ///     Изначально, ASP.NET MVC включает всевозможные движки, для любого языка
+        ///     программирования (.cshtml, .vbhtml). Здесь же происхожит регистрирование
+        ///     только нужного, тем самым получая выигрышь во времени поиска нужного прежставления.
+        /// </summary>
+        private static void ConfigureViewEngines()
+        {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new CSharpRazorViewEngine());
+        }
     }
 }
