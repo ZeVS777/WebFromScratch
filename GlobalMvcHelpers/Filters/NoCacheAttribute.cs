85fb6ac2d9a9755abe64862aedbd5373d5c618ba
ZeVS777 <zevs7772006@gmail.com>
2015-11-09 02:49:40 +0300

    Добавил вспомогательный аттрибут, используемый, чтобы пометить метод, чей вывод не должен кэшироваться

---
+++ GlobalMvcHelpers/Filters/NoCacheAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace GlobalMvcHelpers.Filters
+{
+    /// <summary>
+    /// Аттрибут, используемый, чтобы пометить метод, чей вывод не должен кэшироваться.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public class NoCacheAttribute : OutputCacheAttribute
+    {
+        /// <summary>
+        /// Инициализирует класс <see cref="NoCacheAttribute"/>.
+        /// </summary>
+        public NoCacheAttribute()
+        {
+            NoStore = true;
+            // Duration = 0 по дефолту.
+            // VaryByParam = "*" по дефолту.
+        }
+    }
+}
