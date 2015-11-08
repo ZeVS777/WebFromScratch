907375371bd500dc22f343782131315b18c2576d
ZeVS777 <zevs7772006@gmail.com>
2015-11-08 22:17:19 +0300

    Настройка маркера проверки подлинности при отправке форм на сервер

--- WebFromScratch/Global.asax.cs
+++ WebFromScratch/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using GlobalMvcHelpers.ViewEngines;
 
@@ -11,6 +12,7 @@ namespace WebFromScratch
         protected void Application_Start(object sender, EventArgs e)
         {
             ConfigureViewEngines();
+            ConfigureAntiForgeryTokens();
         }
 
         protected void Session_Start(object sender, EventArgs e)
@@ -45,15 +47,39 @@ namespace WebFromScratch
         #endregion
 
         /// <summary>
-        /// Конфигурирование Движков представлений (View Engines).
-        ///     Изначально, ASP.NET MVC включает всевозможные движки, для любого языка
-        ///     программирования (.cshtml, .vbhtml). Здесь же происхожит регистрирование
-        ///     только нужного, тем самым получая выигрышь во времени поиска нужного прежставления.
+        /// Конфигурирование движков представлений (View Engines).
         /// </summary>
         private static void ConfigureViewEngines()
         {
+            /*     
+            *   Изначально, ASP.NET MVC включает всевозможные движки, для любого языка
+            *   программирования (.cshtml, .vbhtml). Здесь же происхожит регистрирование
+            *   только нужного, тем самым получая выигрышь во времени поиска нужного представления.
+            */
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new CSharpRazorViewEngine());
+        }
+
+        /// <summary>
+        /// Настройка маркера проверки подлинности. Смотри
+        /// https://msdn.microsoft.com/ru-ru/library/system.web.helpers.antiforgery(v=vs.111).aspx
+        /// </summary>
+        private static void ConfigureAntiForgeryTokens()
+        {
+            /*
+            *   Маркер используется при защите от межсайтовой поддерки запроса с форм.
+            *   Смотри https://ru.wikipedia.org/wiki/%D0%9C%D0%B5%D0%B6%D1%81%D0%B0%D0%B9%D1%82%D0%BE%D0%B2%D0%B0%D1%8F_%D0%BF%D0%BE%D0%B4%D0%B4%D0%B5%D0%BB%D0%BA%D0%B0_%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81%D0%B0
+            *   Переименование cookie маркера с "__RequestVerificationToken" на "_fT"
+            *   позволяет добавить защищённости засчёт незнания. К сожалению, нет возможности
+            *   изменить имя формы input маркера, жёстко записан в @Html.AntiForgeryToken и 
+            *   аттрибут ValidationAntiforgeryTokenAttribute как __RequestVerificationToken.
+            *   <input name="__RequestVerificationToken" type="hidden" value="..." />
+            */
+            AntiForgeryConfig.CookieName = "_fT";
+
+            // При включении SSL раскомментировать следующую строку, чтобы убедиться, что
+            // cookie маркера будет требовать SSL. 
+            // AntiForgeryConfig.RequireSsl = true;
         }
     }
 }
