1464c600ff091af1fb0e10a1343d0bd04353166a
ZeVS777 <zevs7772006@gmail.com>
2015-11-08 23:24:13 +0300

    Добавление маршрутов

--- WebFromScratch/App_Start/RouteConfig.cs
+++ WebFromScratch/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Routing;
+﻿using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace WebFromScratch
 {
@@ -6,7 +7,29 @@ namespace WebFromScratch
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            
+            // Нормализация виртуальных путей для оптимизации SEO
+            routes.AppendTrailingSlash = true;
+            routes.LowercaseUrls = true;
+
+            // Игнорирование путей
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.Ignore("css/{*pathInfo}");
+            routes.Ignore("js/{*pathInfo}");
+            routes.Ignore("img/{*pathInfo}");
+            routes.Ignore("error/{*pathInfo}");
+
+            // Включение маршрутов по аттрибутам
+            routes.MapMvcAttributeRoutes();
+
+            // Данный маршрут нужен только для системы логирования Elmah.MVC.
+            // У этой системы есть баг, из-за которого некоторые 404 и 500 ошибки не логируются без 
+            // регистрации такого маршрута.
+            // https://github.com/alexbeletsky/elmah-mvc/issues/60
+            // https://github.com/RehanSaeed/ASP.NET-MVC-Boilerplate/issues/8
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
