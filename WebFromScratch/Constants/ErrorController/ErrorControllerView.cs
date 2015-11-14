1cc3900a10b415cac03cb8c295cacf725dcf0e17
ZeVS777 <zevs7772006@gmail.com>
2015-11-14 23:59:22 +0300

    Система вывода ошибок: Добавил константы контроллера Error

---
+++ WebFromScratch/Constants/ErrorController/ErrorControllerView.cs
@@ -0,0 +1,12 @@
+﻿namespace WebFromScratch.Constants.ErrorController
+{
+    public static class ErrorControllerView
+    {
+        public const string BadRequest = "BadRequest";
+        public const string Forbidden = "Forbidden";
+        public const string InternalServerError = "InternalServerError";
+        public const string MethodNotAllowed = "MethodNotAllowed";
+        public const string NotFound = "NotFound";
+        public const string Unauthorized = "Unauthorized";
+    }
+}
