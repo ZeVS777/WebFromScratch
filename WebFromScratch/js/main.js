9c2b3703e46c4f403e2e6e389c398d1db471e41f
ZeVS777 <zevs7772006@gmail.com>
2015-11-19 02:10:49 +0300

    Шрифты webfontloader

---
+++ WebFromScratch/js/main.js
@@ -0,0 +1,16 @@
+﻿/*
+ * Загружаемые шрифты. Смотри https://github.com/typekit/webfontloader
+ */
+
+WebFontConfig = {
+    google: { families: ['Lobster::latin,cyrillic'] }
+};
+(function () {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
