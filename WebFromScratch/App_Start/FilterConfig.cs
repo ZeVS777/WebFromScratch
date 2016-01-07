70a66ad1467782241a92c9397271f78640f9b1d2
ZeVS777 <zevs7772006@gmail.com>
2016-01-07 03:43:20 +0300

    NoCache заменён на аналог от NWebSec.  Заголовок анти XSS

--- WebFromScratch/App_Start/FilterConfig.cs
+++ WebFromScratch/App_Start/FilterConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.Mvc;
 using System.Web.Routing;
 using GlobalMvcHelpers.Filters;
+using NWebsec.Mvc.HttpHeaders;
 
 namespace WebFromScratch
 {
@@ -8,10 +9,19 @@ namespace WebFromScratch
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            // SEO
             filters.Add(new RedirectToCanonicalUrlAttribute(
                 RouteTable.Routes.AppendTrailingSlash,
                 RouteTable.Routes.LowercaseUrls));
+
+            // HTTPS only
             filters.Add(new RedirectToHttpsAttribute(true));
+
+            // Убрать кеширование страницы.
+            // Cache-Control: no-cache, no-store, must-revalidate Expires: -1 Pragma: no-cache
+            // Возможно следует запретить кеширование только на специфичных сраницах,
+            // таких как платёжная система
+            filters.Add(new SetNoCacheHttpHeadersAttribute());
         }
     }
 }
--- WebFromScratch/HttpModules/RemoveServerResponseHeader.cs
+++ WebFromScratch/HttpModules/RemoveServerResponseHeader.cs
@@ -30,6 +30,11 @@ namespace WebFromScratch.HttpModules
         /// <param name="e"></param>
         void OnPreSendRequestHeaders(object sender, EventArgs e)
         {
+            // как говориться здесь: http://www.asp.net/aspnet/overview/web-development-best-practices/what-not-to-do-in-aspnet-and-what-to-do-instead#presend
+            // использование OnPreSendRequestHeaders не желательно
+            // нужны другие решение такие, как native-code module для IIS
+            // здесь так реализовано лишь для того, чтобы показать, что для защиты по не знанию
+            // заголовок Server не должен сообщать о реально используемых технологиях.
             HttpContext.Current.Response.Headers.Remove("Server");
         }
     }
