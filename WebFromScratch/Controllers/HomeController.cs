044a2200d83c87b266e9e5d42cc6d30830fe8154
ZeVS777 <zevs7772006@gmail.com>
2016-01-07 23:35:36 +0300

    Добавил тестовый метод получения страниц ошибок

--- WebFromScratch/Controllers/HomeController.cs
+++ WebFromScratch/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
 using WebFromScratch.Constants;
 
 namespace WebFromScratch.Controllers
@@ -9,6 +11,23 @@ namespace WebFromScratch.Controllers
         public ActionResult Index()
         {
             return View(HomeControllerView.Index);
+        }
+
+        [Route("home/error/{status}")]
+        public ActionResult Error(int status)
+        {
+            switch (status)
+            {
+                case 403:
+                    Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                    return Content("Oops 403");
+                case 404:
+                    return HttpNotFound("Oops 404!");
+                case 500:
+                default:
+                    throw new Exception("Oops 500!");
+
+            }
         }
 
         [Route("about", Name = HomeControllerRoute.GetAbout)]
